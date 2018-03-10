99bee88c4598ebf5162cef8bc46c08d77dddb10b
lhchavez <lhchavez@lhchavez.com>
2018-03-10 15:41:38 +0000

    Test packet object limit
    This test ensures that gitbombs can be handled.

--- server_test.go
+++ server_test.go
@@ -168,3 +168,82 @@ func TestServerPush(t *testing.T) {
 		t.Errorf("Failed to clone: %v %q", err, output)
 	}
 }
+
+func TestGitbomb(t *testing.T) {
+	gitcmd, err := exec.LookPath("git")
+	if err != nil {
+		t.Skipf("git not found: %v", err)
+	}
+
+	dir, err := ioutil.TempDir("", "server_test")
+	if err != nil {
+		t.Fatalf("Failed to create directory: %v", err)
+	}
+	log := log15.New()
+	if os.Getenv("PRESERVE") != "" {
+		log.Info("Preserving test directory", "path", dir)
+	} else {
+		defer os.RemoveAll(dir)
+	}
+
+	{
+		repo, err := git.InitRepository(filepath.Join(dir, "repo.git"), true)
+		if err != nil {
+			t.Fatalf("Failed to initialize git repository: %v", err)
+		}
+		repo.Free()
+	}
+
+	handler := GitServer(dir, true, nil, nil, nil, log)
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+
+	repoDir := filepath.Join(dir, "repo")
+	upstreamURL := ts.URL + "/repo/"
+
+	cmd := exec.Command(gitcmd, "clone", "--depth=1", upstreamURL, repoDir)
+	cmd.Stdin = strings.NewReader("foo\nbar\n")
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Fatalf("Failed to run git clone: %v %q", err, output)
+	}
+
+	cmd = exec.Command(gitcmd, "remote", "get-url", "--push", "origin")
+	cmd.Dir = repoDir
+	if output, err := cmd.CombinedOutput(); err != nil || !strings.HasPrefix(string(output), upstreamURL) {
+		t.Errorf("Failed to clone: %v %q", err, string(output))
+	}
+
+	if err = ioutil.WriteFile(filepath.Join(repoDir, "empty"), []byte{}, 0644); err != nil {
+		t.Fatalf("Failed to create empty file: %v", err)
+	}
+
+	cmd = exec.Command(gitcmd, "add", "empty")
+	cmd.Dir = repoDir
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("Failed to clone: %v %q", err, output)
+	}
+
+	cmd = exec.Command(gitcmd, "commit", "--all", "--message", "Empty")
+	cmd.Dir = repoDir
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("Failed to clone: %v %q", err, output)
+	}
+
+	cmd = exec.Command(gitcmd, "show")
+	cmd.Dir = repoDir
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("Failed to clone: %v %q", err, output)
+	}
+
+	cmd = exec.Command(gitcmd, "push", "--porcelain", "-u", "origin", "HEAD:changes/initial")
+	cmd.Dir = repoDir
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("Failed to clone: %v %q", err, output)
+	}
+
+	cmd = exec.Command(gitcmd, "push", "--porcelain", "-u", "origin", "HEAD:master")
+	cmd.Dir = repoDir
+	if output, err := cmd.CombinedOutput(); err != nil {
+		t.Errorf("Failed to clone: %v %q", err, output)
+	}
+}
